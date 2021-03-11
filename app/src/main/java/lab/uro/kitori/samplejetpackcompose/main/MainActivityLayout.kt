dc4c55b1578a8b820769a2fca5a08b0125079ec3
urotarou <devak9kflab@gmail.com>
2021-03-11 20:37:47 +0900

    起動時画面をScrollに対応

--- app/src/main/java/lab/uro/kitori/samplejetpackcompose/main/MainActivityLayout.kt
+++ app/src/main/java/lab/uro/kitori/samplejetpackcompose/main/MainActivityLayout.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -35,9 +37,12 @@ fun ScreenLayout(
     sampleListButtonOnClick: () -> Unit = {}
 ) {
     RootScreenLayout(darkTheme, "Main") {
+        val scrollState = rememberScrollState()
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(scrollState)
                 .padding(32.dp)
         ) {
             Button(
