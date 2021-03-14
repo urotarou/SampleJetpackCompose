b4489888b94183495625765c147d4076195cf635
urotarou <devak9kflab@gmail.com>
2021-03-14 21:56:28 +0900

    WebViewのurl変更

--- app/src/main/java/lab/uro/kitori/samplejetpackcompose/sampleandroidview/SampleAndroidViewActivityLayout.kt
+++ app/src/main/java/lab/uro/kitori/samplejetpackcompose/sampleandroidview/SampleAndroidViewActivityLayout.kt
@@ -2,15 +2,10 @@ package lab.uro.kitori.samplejetpackcompose.sampleandroidview
 
 import android.webkit.WebView
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import lab.uro.kitori.samplejetpackcompose.compose.ui.BranchScreen
 
@@ -36,18 +31,11 @@ fun ScreenLayout(
         "Sample AndroidView",
         backIconOnClick
     ) {
-        Column(
+        AndroidView(
+            factory = { WebView(it) },
             modifier = Modifier.fillMaxSize()
         ) {
-            AndroidView(
-                factory = { WebView(it) },
-                modifier = Modifier.fillMaxSize()
-            ) {
-                it.loadUrl("https://www.google.com/")
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text("show AndroidView")
+            it.loadUrl("https://github.com/urotarou/SampleJetpackCompose/blob/master/app/src/main/java/lab/uro/kitori/samplejetpackcompose/sampleandroidview/SampleAndroidViewActivityLayout.kt")
         }
     }
 }
