85a6fec0a676d9e3b973ab486c4fcbf68db5458f
urotarou <devak9kflab@gmail.com>
2021-09-16 02:24:44 +0900

    FAB画面の土台を置き換え

--- app/src/main/java/lab/uro/kitori/samplejetpackcompose/samplefab/SampleFabScreen.kt
+++ app/src/main/java/lab/uro/kitori/samplejetpackcompose/samplefab/SampleFabScreen.kt
@@ -1,14 +1,22 @@
 package lab.uro.kitori.samplejetpackcompose.samplefab
 
+import android.widget.Toast
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import lab.uro.kitori.samplecore.screen.BranchWithFabScreen
+import androidx.compose.ui.unit.dp
+import lab.uro.kitori.samplecore.screen.AppScreen
 
 @Preview
 @Composable
@@ -27,7 +35,9 @@ fun ScreenLayout(
     darkTheme: Boolean = isSystemInDarkTheme(),
     backIconOnClick: () -> Unit = {}
 ) {
-    BranchWithFabScreen(
+    val context = LocalContext.current
+
+    AppScreen(
         darkTheme,
         "Sample FAB",
         backIconOnClick
@@ -39,6 +49,17 @@ fun ScreenLayout(
                 "FAB",
                 modifier = Modifier.align(Alignment.Center)
             )
+
+            FloatingActionButton(
+                modifier = Modifier
+                    .align(alignment = Alignment.BottomEnd)
+                    .padding(0.dp, 0.dp, 16.dp, 16.dp),
+                onClick = {
+                    Toast.makeText(context, "click fab", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "fab")
+            }
         }
     }
 }
